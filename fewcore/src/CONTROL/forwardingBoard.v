1edf987c3de1fdb459e56c545f5b267733865387
João Gabriel <joaogabrieloliveira9@gmail.com>
2018-12-04 14:45:55 -0200

    correção controle

--- fewcore/src/CONTROL/forwardingBoard.v
+++ fewcore/src/CONTROL/forwardingBoard.v
@@ -1,7 +1,7 @@
 module forwardingBoard(lastRd,rs1,rs2,encFB);
 	input [4:0] lastRd,rs1,rs2;
-	output [1:0] encFB;
+	output encFB_1, encFB_2;
 	
-	assign encFB[0] = (lastRd == rs1 && rs1 != 0);
-	assign encFB[1] = (lastRd == rs2 && rs2 != 0);
+	assign encFB_1 = (lastRd == rs1 && rs1 != 0);
+	assign encFB_2 = (lastRd == rs2 && rs2 != 0);
 endmodule
--- fewcore/src/control.v
+++ fewcore/src/control.v
@@ -4,24 +4,27 @@ module control (clk, reset, lastRD, curRS1, curRS2, ALU_forwarding_RS1, ALU_forw
 
 input clk, reset;
 input [4:0] lastRD, curRS1, curRS2;
-output ALU_forwarding_RS1, ALU_forwarding_RS2;
+output reg ALU_forwarding_RS1, ALU_forwarding_RS2;
 
 reg [4:0] i_lastRD;
 
-reg [1:0] FB;
+wire i_FB_1;
+wire i_FB_2;
 
 // isso acontece na borda de subida
 always @(negedge clk) begin
 	if (reset) i_lastRD <= 5'b0;
 	else i_lastRD <= lastRD;
-	{ALU_forwarding_RS1, ALU_forwarding_RS2} <= FB;
+	{ALU_forwarding_RS1, ALU_forwarding_RS2} <= {i_FB_1, i_FB_2};
 end
+
 // isso acontece na borda de descida
 forwardingBoard forwarding_board_m(
 	.lastRd(i_lastRD),
 	.rs1(curRS1),
 	.rs2(curRS2),
-	.encFB(FB)
+	.encFB_1(i_FB_1)
+	.encFB_2(i_FB_2)
 );
 
 
