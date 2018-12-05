d2be33e7c398204de5e348ce04f0a7d621ee3907
Elves <elvemateusrodrigues@outlook.com>
2018-12-05 15:33:46 -0200

    Alterações no controle

--- fewcore/src/control.v
+++ fewcore/src/control.v
@@ -6,21 +6,17 @@ input clk, reset;
 input [4:0] lastRD, curRS1, curRS2;
 output reg ALU_forwarding_RS1, ALU_forwarding_RS2;
 
-reg [4:0] i_lastRD;
-
 wire i_FB_1;
 wire i_FB_2;
 
 // isso acontece na borda de subida
 always @(negedge clk) begin
-	if (reset) i_lastRD <= 5'b0;
-	else i_lastRD <= lastRD;
-	{ALU_forwarding_RS1, ALU_forwarding_RS2} <= {i_FB_1, i_FB_2};
+	ALU_forwarding_RS1 <= i_FB_1;
+	ALU_forwarding_RS2 <= i_FB_2;
 end
-
 // isso acontece na borda de descida
 forwardingBoard forwarding_board_m(
-	.lastRd(i_lastRD),
+	.lastRd(lastRD),
 	.rs1(curRS1),
 	.rs2(curRS2),
 	.encFB_1(i_FB_1),
